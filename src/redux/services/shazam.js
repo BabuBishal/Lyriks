42ee7e273b82f1d64df4ec97071510f6fb39bd7c
Bishal <bohorabishal20@gmail.com>
2024-04-07 19:21:25 +0545

    Api fetch

---
+++ src/redux/services/shazam.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+    export const shazamApi = createApi({
+        reducerPath: 'shazamApi',
+        baseQuery: fetchBaseQuery({
+          baseUrl: 'https://shazam.p.rapidapi.com',
+          prepareHeaders: () => {
+            const headers = new Headers();
+            headers.set('X-RapidAPI-key', '7497862f94mshe2ac510022a674dp1e3c55jsna47c2a1a46e5');
+
+            return headers;
+          },
+        }),
+        endpoints: (builder) => ({
+          getTopCharts: builder.query({ query: () => '/charts/track'})
+        })
+    });
+
+    export const {
+      useGetTopChartsQuery,
+    } = shazamApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import playerReducer from './features/playerSlice';
+import { shazamApi } from './services/shazam';
 
 export const store = configureStore({
   reducer: {
+    [shazamApi.reducerPath]: shazamApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false, // Disable serializable check for API slices
+  }).concat(shazamApi.middleware),
 });
