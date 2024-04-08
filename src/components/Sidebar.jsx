6007bd5d663479591b886d7054f1dc15c32c96ef
Bishal <bohorabishal20@gmail.com>
2024-04-08 22:32:05 +0545

    sidebar start

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,20 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { RiCloseLine } from 'react-icons/ri'
+import {logo} from '../assets'
+import { links } from '../assets/constants'
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  
+  return(
+    <>
+    <div>
+      <img />
+    </div>
+    </>
+  )
+
+}
 
 export default Sidebar;
