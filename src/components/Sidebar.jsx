e770e6ddbb3f514203c956f8e7f56ecd414a9cec
Bishal <bohorabishal20@gmail.com>
2024-04-08 23:32:15 +0545

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,15 +3,45 @@ import { NavLink } from 'react-router-dom'
 import { RiCloseLine } from 'react-icons/ri'
 import {logo} from '../assets'
 import { links } from '../assets/constants'
+import { HiOutlineMenu} from 'react-icons/hi'
+
+const NavLinks = () = (
+<div className='mt-10'>
+  {links.map((item) =>{
+    <NavLink
+     key={item.name}
+     to={item.to}
+     className='flex flex-row justify-start items-center my-8 text-sm font'
+     onClick={() => handleClick && handleClick()}
+    >
+      <item.icon className='w-6 h-6 mr-3' />
+      {item.name}
+    </NavLink>
+  })}
+  
+</div>
+)
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   
   return(
     <>
-    <div>
-      <img />
+    <div className='md:flex hidden flex-col w-[240px] py-10 px=4 bg=[#191624'>
+      <img src={logo} alt='logo' className='w-full h-14 object-contain' />
     </div>
+    <div className='absolute md:hiddwn block top-6 right-3' >
+      {mobileMenuOpen ? ( 
+        <RiCloseLine className='w-6 h-6 text-white mr-2'/>
+        ) : 
+        <HiOutlineMenu className='w-6 h-6 text-white mr-2'
+        onClick={() => setMobileMenuOpen(true)} />}
+    </div>
+    <div className={`absolute top-0 h-screen w-2/3 bg-gradient=to=tl from-white/10 to-[#483d8b]
+      backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0': '-left-full'}`}>
+      <img src={logo} alt='logo' className='w-full h-14 object-contain' />
+      <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+    </div> 
     </>
   )
 
