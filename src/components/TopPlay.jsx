3df6347f027da3b4889f2ccacede407582aa79e7
Bishal <bohorabishal20@gmail.com>
2024-04-09 21:26:19 +0545

    Starting top songs

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,37 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { freeMode } from 'swiper'
+
+import PlayPause from './PlayPause'
+import { playPause, setActiveSong } from '../redux/features/playerSlice'
+import { useGetTopChartQuery } from '../redux/services/shazam'
+
+
+const TopPlay = () => {
+ 
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useRef(null);
+
+  const topPlays = data?.slice(0,5)
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({song, data, i}));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div ref={divRef}>
+
+    </div>
+  )
+
+}
 
 export default TopPlay;
