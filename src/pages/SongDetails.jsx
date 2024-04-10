09325af3c3739353eb80ed5fb9d880465c169168
Bishal <bohorabishal20@gmail.com>
2024-04-10 22:14:03 +0545

    fix

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,23 +3,27 @@ import { useSelector, useDispatch } from 'react-redux'
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components'
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-
-const { data, isFetching, error } = useGetTopChartsQuery();
-   console.log(data)
-
-    if(isFetching) return <Loader title='Loading songs...' />;
-
-    if (error) return <Error />
+import { useGetSongDetailsQuery } from '../redux/services/shazam';
 
 const SongDetails = () => {
     const dispatch = useDispatch();
     const { songid } = useParams();
     const { activeSong, isPlaying } = useSelector((state) => state.player);
 
+    const { data : songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+    console.log(songData)
+ 
 return (
-    <div>
-        <div>
-            <h3></h3>
+    <div className='flex flex-col'>
+        <div className='mb-10'>
+            <h2 className='text-white text-3xl font-bold'>Lyrics:</h2>
+            <div className='mt-5'>
+                { songData?.sections[1].type === 'LYRICS' ?
+                    songData?.sections[1].text.map((line, i) => (
+                    <p className='text-gray text-base my-1'>{line}</p>
+                )) : 
+                    <p className='text-gray text-base my-1'>Sorry, No lyrics found!</p>}
+            </div>
         </div>
     </div>
 )
--- src/redux/services/shazam.js
+++ src/redux/services/shazam.js
@@ -13,11 +13,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
           },
         }),
         endpoints: (builder) => ({
-          getTopCharts: builder.query({ query: () => '/charts/track'})
+          getTopCharts: builder.query({ query: () => '/charts/track'}),
+          getSongDetails: builder.query({ query: ({ songid }) => `/songs/v2/get-details?track_id=${songid}`}),
+
         }),
-        endpoints: (builder) => ({
-          getSongDetails: builder.query({ query: ({ songid }) => `/songs/v2/get-details?track_id=${songid}'`})
-        }),
+      
     });
 
     export const {
